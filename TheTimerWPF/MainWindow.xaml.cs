3a8ab8b2de2fdd267c0fb1df5f6a13316f2dedbb
iceberg <wladimir.melnik@gmail.com>
2020-05-06 00:18:53 +0200

    add check timer accuracy

--- TheTimerWPF/MainWindow.xaml.cs
+++ TheTimerWPF/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TheTimerWPF
 {
@@ -23,6 +15,50 @@ namespace TheTimerWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        Timer _mainTimer = new Timer();
+        Timer _secondsTimer = new Timer();
+
+        private void StartTimerButtonClick(object sender, RoutedEventArgs e)
+        {
+            int min = int.Parse(Mins.Text);
+            int sec = int.Parse(Secs.Text);
+
+            var period = new TimeSpan(0, min, sec);
+
+            _mainTimer.Interval = period.TotalMilliseconds;
+            _mainTimer.Elapsed += MainTimerElapsed;
+            _mainTimer.AutoReset = false;
+
+            _secondsTimer.Interval = 1000;
+            _secondsTimer.Elapsed += SecondsTimerElapsed;
+
+            _mainTimer.Start();
+            _secondsTimer.Start();
+        }
+
+        private void SecondsTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            MessageBox.Show($"Time is up: {Stopwatch.GetTimestamp()}/{TimeSpan.TicksPerSecond}", "SecondsTimer");
+        }
+
+        private void MainTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            _secondsTimer.AutoReset = false;
+            _secondsTimer.Enabled = false;
+            _mainTimer.Enabled = false;
+            MessageBox.Show($"Time is up: {Stopwatch.GetTimestamp()}/{TimeSpan.TicksPerSecond}", "MainTimer");
+        }
+
+        private void PreviewTimeTextboxKeyDown(object sender, KeyEventArgs e)
+        {
+            //add normal check for digits and for editing key inclusive Tab
+            //if (((e.Key >= Key.D0 && e.Key <= Key.D9) || e.Key == Key.Clear || e.Key == Key.Delete) && 
+            //    ((TextBox)sender).Text.Length < 2)
+            //    return;
+            
+            //e.Handled = true;
         }
     }
 }
